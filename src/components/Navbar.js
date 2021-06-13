b61f34cf47c14e18309b27ded3c1aa833f6bb60f
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-13 13:26:57 +0530

    add

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { data } from '../data';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div className="nav">
+                <div className="search-container">
+                    <input/>
+                    <button id="search-btn">Search</button>
+                </div>
+            </div>
+        )
+    }
+}
