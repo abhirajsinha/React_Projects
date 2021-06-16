c6616f168decbc21f1f9e67b6f31668ed3dce840
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-16 12:41:17 +0530

    added stores

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from 'redux';
+
 import "./index.css";
 import App from "./components/App";
+import movies from "./reducers";
 
+const store=createStore(movies);
+console.log('store',store);
+console.log('BeforeState',store.getState());
+
+store.dispatch({
+    type: 'ADD_MOVIES',
+    movies:[{name: 'Superman'}]
+});
+
+console.log('AfterState',store.getState());
 ReactDOM.render(<App />, document.getElementById("root"));
