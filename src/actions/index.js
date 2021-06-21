c6eb13b4f1baef23d55ea44d76965f19c4a48d9b
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-21 21:07:12 +0530

    added active functionality in movies and favourites button

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,25 +1,33 @@
-export const ADD_MOVIES = 'ADD_MOVIES';
-export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
-export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
+export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_TO_FAVOURITES = "ADD_TO_FAVOURITES";
+export const REMOVE_FROM_FAVOURITES = "REMOVE_FROM_FAVOURITES";
+export const SET_SHOW_FAVOURITES = "SET_SHOW_FAVOURITES";
 
 //Action creators
-export function addMovies(movies){
-    return {
-        type:'ADD_MOVIES',
-        movies:movies
-      }
-} 
-
-export function addfavourite(movie){
+export function addMovies(movies) {
   return {
-      type:'ADD_TO_FAVOURITES',
-      movie:movie
-    }
-} 
+    type: "ADD_MOVIES",
+    movies: movies,
+  };
+}
 
-export function removeFromFavourites(movie){
+export function addfavourite(movie) {
   return {
-      type:'REMOVE_FROM_FAVOURITES',
-      movie:movie
-    }
-} 
+    type: "ADD_TO_FAVOURITES",
+    movie: movie,
+  };
+}
+
+export function removeFromFavourites(movie) {
+  return {
+    type: "REMOVE_FROM_FAVOURITES",
+    movie: movie,
+  };
+}
+
+export function setShowFavourites(val) {
+  return {
+    type: "SET_SHOW_FAVOURITES",
+    val: val,
+  };
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import {
   ADD_MOVIES,
   ADD_TO_FAVOURITES,
   REMOVE_FROM_FAVOURITES,
+  SET_SHOW_FAVOURITES
 } from "../actions";
 
 const initalMoviesState = {
   list: [],
   favourites: [],
+  showFavourites:false
 };
 export default function movies(state = initalMoviesState, action) {
   // if (action.type === ADD_MOVIES) {
@@ -40,6 +42,12 @@ export default function movies(state = initalMoviesState, action) {
         ...state,
         favourites: filteredArray,
       };
+
+      case SET_SHOW_FAVOURITES:
+      return {
+        ...state,
+        showFavourites:action.val
+      }
 
     default:
       return state;
