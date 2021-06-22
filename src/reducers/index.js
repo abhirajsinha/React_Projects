d743dc11cfd9794472bfa6cb382ef21011e54701
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-22 11:13:22 +0530

    Used React Combined Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import { combineReducers } from "redux";
 import {
   ADD_MOVIES,
   ADD_TO_FAVOURITES,
@@ -67,9 +68,13 @@ const initialRootState={
   search:initialSearchState 
 }
 
-export default function rootReducer(state=initialRootState,action){
-  return {
-    movies:movies(state.movies,action),
-    search:search(state.search,action)
-  }
-}
+// export default function rootReducer(state=initialRootState,action){
+//   return {
+//     movies:movies(state.movies,action),
+//     search:search(state.search,action)
+//   }
+// }
+export default combineReducers({
+  movies:movies,
+  search:search
+})
